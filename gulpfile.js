c4f40adc4d0efe16866f5a1b19cdde5aee675021
vgvenkat <vgkrish619@gmail.com>
2017-02-23 13:20:01 -0800

    Steps towards progress

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ var uglify = require('gulp-uglify')
 var cssnano = require('gulp-cssnano')
 var del = require('del')
 var pump = require('pump')
-var minify = require('html-minifier').minify
+var inlineCss = require('gulp-inline-css')
+var minify = require('gulp-htmlmin')
+var gulpif = require('gulp-if')
+var imagemin = require('gulp-imagemin');
 var site = '',
   port = 3000
 gulp.task('default', function () {
@@ -19,11 +22,20 @@ gulp.task('connect', function () {
   	port: port
   	})
 })
+gulp.task('connect:dist',['folder-setup'], function () {
+  connect.server({
+  	port: port,
+    root:'dist'
+  	})
+})
 gulp.task('clean-dist', function () {
   return del(['dist/**'])
 })
 gulp.task('html-minify', function () {
-
+  return gulp.src('./**.html')
+        .pipe(inlineCss())
+        .pipe(minify({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
 })
 gulp.task('css-min', function () {
   return gulp.src('css/*.css')
@@ -41,11 +53,27 @@ gulp.task('js-min', function (cb) {
     cb
   )
 })
+gulp.task('image-min', function(){
+  return gulp.src('./img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+})
+gulp.task('views', function(){
+  return gulp.src('./views/**/**')
+          .pipe(gulpif('*.css',cssnano()))
+          .pipe(gulpif('*.js',uglify()))
+          .pipe(gulpif('*/images/*',imagemin()))
+          .pipe(gulpif('*.html', inlineCss()))
+          .pipe(gulp.dest('dist/views'))
+})
 gulp.task('folder-setup', function () {
   return sequence(
 		'clean-dist',
 		'css-min',
-		'js-min'
+		'js-min',
+    'image-min',
+    'html-minify',
+    'views'
 		)
 })
 gulp.task('ngrok-url', function (cb) {
